baab5239b8fa02a7ba883f26285e1356caaf85cd
sofia <sofia.leivapires@gmail.com>
2026-04-24 16:44:03 -0300

    inserindo descobrir filmes

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const apiKey = '530b6b9d5566bdfb61e043fe04d419c7';
 const inputFilme = document.getElementById("filme");
 const botao = document.getElementById("botao");
 const result = document.getElementById("result");
+const botaoDescobrir = document.getElementById("botaoDescobrir");
 
 
 botao.addEventListener("click", async () => {
@@ -22,13 +23,63 @@ botao.addEventListener("click", async () => {
     const resposta = await fetch(url);
     const dados = await resposta.json();
 
-    console.log(`🎬 Resultados para: "${inputValue}"\n`);
+    result.innerHTML = "";
+
+    dados.results.forEach(filme => {
+
+      const card = document.createElement("div");
+      card.className = "bg-white p-2 border border-gray-300 rounded-xl shadow-lg flex flex-col items-center gap-8";
+
+      const linkCapa = `https://image.tmdb.org/t/p/w500${filme.poster_path}`;
+
+      const poster = document.createElement("img");
+      poster.src = linkCapa
+      poster.className = "w-50 h-60 rounded-lg shadow-xl hover:scale-105 transition-transform duration-300"
+      card.appendChild(poster); 
+
+      const tituloFilme= document.createElement("h2");
+      tituloFilme.innerHTML = `Título: ${filme.title}`;
+      card.appendChild(tituloFilme);
+
+      const nota= document.createElement("p");
+      nota.innerHTML = `Nota: ⭐ ${filme.vote_average} ` ;
+      card.appendChild(nota);
+
+      const sinopse= document.createElement("p");
+      sinopse.innerHTML = `Sinopse: ${filme.overview}`;
+      sinopse.className = "text-justify ml-5 mr-5"
+      card.appendChild(sinopse);
+        
+      result.appendChild(card);
+      
+    });
+
+
+  } catch (erro) {
+    console.error("Ops, falha ao conectar com a TMDB:", erro);
+  }
+
+
+})
+
+
+botaoDescobrir.addEventListener("click", async () => {
+ 
+  inputFilme.value = "";
+
+
+  const urlDescobrir = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=pt-BR&sort_by=popularity.desc&page=1`;
+
+  try {
+    const resposta = await fetch(urlDescobrir);
+    const dados = await resposta.json();
 
     result.innerHTML = "";
 
     dados.results.forEach(filme => {
 
       const card = document.createElement("div");
+
       card.className = "bg-white p-2 border border-gray-300 rounded-xl shadow-lg flex flex-col items-center gap-8";
 
       const linkCapa = `https://image.tmdb.org/t/p/w500${filme.poster_path}`;
